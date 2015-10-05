97039d8ea53bd8de84973613c163ad11beb68bdc
ckranz <chris@kranz.com>
2015-10-05 18:43:10 +0100

    Updated upload.php with slight tweaks to support the work on process.php Process.php has been added to, struggling a little with now handling the logic of converting flat CSV data to relational JSON data.

--- process.php
+++ process.php
@@ -4,7 +4,68 @@
  </head>
  <body>
 <?php
-//Do sexy stuff with data
+foreach($_POST['columns'] AS $key => $value)
+{
+	if($value != "ignore")
+	{
+		echo $key . " : " . $value ."<br />";
+		if(isset($structure[$value]))
+		{
+			$structure[$value] = $structure[$value].",".$key;
+		}else{
+			$structure[$value] = $key;
+		}
+	}
+}
+foreach($_POST AS $value)
+{
+	echo "<!-- $value //-->\r\n";
+}
+$filename = "uploads/".basename($_POST['UploadedFile']);
+$row = 1;
+ini_set('auto_detect_line_endings',TRUE);
+echo "<!-- $filename //-->";
+if (($handle = fopen($filename, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        for ($c=0; $c < $num; $c++) {
+            if($row==1)
+            {
+                $columns[]=$data[$c];
+            }else{
+		$filedata[$columns[$c]][]=$data[$c];
+	    }
+        }
+        $row++;
+    }
+    fclose($handle);
+}
+echo "<pre>";
+for($i=count($structure)-1;$i>=0;$i--)
+{
+    $arrName = "Arr$i";
+    $records = split(",",$structure[$i]);
+    echo $records[0]."<br />";
+    foreach($filedata AS $key => $value)
+    {
+	$keyval = $records[0];
+	if(in_array($key, $records))
+	{
+	    foreach($value AS $num => $data)
+	    {
+		if($key != $keyval)
+		    ${$arrName}[$filedata[$keyval][$num]][$key] = $data;
+	    }
+	}
+    }
+    unset($keyval);
+    echo "Array Name: $arrName<br />";
+    print_r(${$arrName});
+}
+print_r($tempArray);
+print_r($structure);
+//print_r($filedata);
+echo "</pre>";
 ?>
  </body>
 </html>
